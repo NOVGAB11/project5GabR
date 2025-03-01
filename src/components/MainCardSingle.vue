c269dc88bb758e1a30d10c1ed9bf5a703bffee88
Gabriela Rodriguez <grodri41@stu.southtexascollege.edu>
2025-03-01 17:03:08 -0600

    space

--- src/components/MainCardSingle.vue
+++ src/components/MainCardSingle.vue
@@ -22,7 +22,7 @@
 <template>
     <div>
     <h1 class="text-2xl font-bold text-center text-green-600">{{ recipeTitle }}</h1>
-    <img :src="foodImage" alt="Food Image" class="mx-auto my-6 w-96 h-96 object-cover rounded-xl shadow-lg"/>
+    <img v-bind:src="foodImage" alt="Food Image" class="mx-auto my-6 w-96 h-96 object-cover rounded-xl shadow-lg"/>
 
     <p class="text-lg font-medium text-green-600">>Prep Time: <span class="font-normal text-green-500">{{ prepTime }}</span></p>
     <p class="text-lg font-medium text-green-600">Cooking Time: <span class="font-normal text-green-500">{{ cookTime }}</span></p>
@@ -38,7 +38,6 @@
       <li v-for="(step, index) in instructions" :key="index">{{ step }}</li>
     </ol>
 
-    <h3 class="text-2xl font-bold text-center text-green-600">Owner of Recipe </h3>
     <p class="text-lg font-medium text-green-800">This recipe was created by: {{ firstname }} {{ lastname }}</p>
     <p class="text-lg font-medium text-green-800">Username: {{ username }}</p>
     <p class="text-lg font-medium text-green-800">Bio: {{ bio }}</p>
