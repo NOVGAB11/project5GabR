8c8bb56a3b0d93cb9f91792b741d06790c2f9eba
Gabriela Rodriguez <grodri41@stu.southtexascollege.edu>
2025-03-01 16:58:24 -0600

    Testing out recipe idea, added tailwind, tmeplate still giving me vulnerability issues and does not show up properly on the webpage

--- src/components/MainCardSingle.vue
+++ src/components/MainCardSingle.vue
@@ -1,10 +1,42 @@
 <script setup>
     import { faker } from '@faker-js/faker'
-    const firstname = faker.person.firstName()
-    const lastname = faker.person.lastName()
+    const recipeTitle = faker.lorem.words(4); 
+    const prepTime = faker.datatype.number() + ' minutes'; 
+    const cookTime = faker.datatype.number() + ' minutes'; 
+    const servingSize = faker.datatype.number(); 
 
+     const ingredients = Array.from({ length: 7 }, () => {
+         return faker.datatype.number() + 'g ' + faker.food.ingredient();
+     });
+
+    const instructions = Array.from({ length: 6 }, () => faker.lorem.sentence());
+
+    const firstname = faker.person.firstName();
+    const lastname = faker.person.lastName();
+    const username = faker.internet.userName({ firstName: firstname, lastName: lastname }).toLowerCase();
+    const bio = faker.lorem.lines(2);
 </script>
 
 <template>
-    {{ firstname }} {{ lastname }} <br/>
+    <div>
+    <h1 class="text-2xl font-bold text-center text-green-600">{{ recipeTitle }}</h1>
+    <p class="text-lg font-medium text-green-600">>Prep Time: <span class="font-normal text-green-500">{{ prepTime }}</span></p>
+    <p class="text-lg font-medium text-green-600">Cooking Time: <span class="font-normal text-green-500">{{ cookTime }}</span></p>
+    <p class="text-lg font-medium text-green-600">Number of Servings: <span class="font-normal text-green-500"> {{ servings }}</span></p>
+
+    <h2 class="text-2xl font-bold text-center text-green-600">Your Ingredients</h2>
+    <ul>
+      <li v-for="ingredient in ingredients" :key="ingredient">{{ ingredient }}</li>
+    </ul>
+
+    <h2 class="text-2xl font-bold text-center text-green-600">Cooking Instructions : </h2>
+    <ol>
+      <li v-for="(step, index) in instructions" :key="index">{{ step }}</li>
+    </ol>
+
+    <h3 class="text-2xl font-bold text-center text-green-600">Owner of Recipe </h3>
+    <p class="text-lg font-medium text-green-800">This recipe was created by: {{ firstname }} {{ lastname }}</p>
+    <p class="text-lg font-medium text-green-800">Username: {{ username }}</p>
+    <p class="text-lg font-medium text-green-800">Bio: {{ bio }}</p>
+  </div>
 </template>
